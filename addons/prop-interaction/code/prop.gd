ad1a68fc2b5c6b34e18950c82be618cef4223f68
paulocomp <paulocorreacomp@gmail.com>
2026-03-01 13:28:46 -0300

    refactor: remove adapter and simplify to RigidBody3D

--- addons/prop-interaction/code/prop.gd
+++ addons/prop-interaction/code/prop.gd
@@ -3,31 +3,29 @@
 # Class: Prop
 # Purpose: []
 #
+# Signals:
+#   -
+#
 # Attributes:
 #   - target_body: Node3D >
 #   - body_layer: int     >
 #
 # Methods:
-#   -
+#   - get_target_body() >
 #
 ################################################################################
 
 class_name Prop
 extends Node
 
-@export var target_body : Node3D
+@export var target_body : RigidBody3D
 @export var body_layer : int = 3
 
 
 func _ready() -> void:
 	get_target_body()
-	get_adapter()
 
 
 func get_target_body():
 	if target_body == null:
 		target_body = get_child(0)
-
-
-func get_adapter() -> Resource:
-	return null
