8138a1598ea7d36cbcaa621ad8a1256c535d4d0f
paulocomp <paulocorreacomp@gmail.com>
2026-03-05 15:06:28 -0300

    feat: Add throw behaviour

--- addons/prop-interaction/code/components/throw_behaviour.gd
+++ addons/prop-interaction/code/components/throw_behaviour.gd
@@ -1,6 +1,15 @@
 class_name ThrowBehaviour
 extends PropBehaviour
 
+@export var throw_force: float = 8.0
+
 
 func execute() -> void:
-	pass
+	throw(throw_force)
+	prop.request_interaction_end()
+
+
+func throw(force: float) -> void:
+	var dir: Vector3 = -camera.global_transform.basis.z
+	prop.target_body.linear_velocity = Vector3.ZERO
+	prop.target_body.apply_central_impulse(dir.normalized() * force)
