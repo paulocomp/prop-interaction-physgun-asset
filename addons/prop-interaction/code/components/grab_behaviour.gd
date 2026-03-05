488887f0b0a625337a83f028d4662fd1f84bf7a9
paulocomp <paulocorreacomp@gmail.com>
2026-03-05 15:05:25 -0300

    feat: Add approx and recede sub-behaviours

--- addons/prop-interaction/code/components/grab_behaviour.gd
+++ addons/prop-interaction/code/components/grab_behaviour.gd
@@ -3,27 +3,77 @@ extends PropBehaviour
 
 @export var spring_strength: float = 120.0
 @export var grab_damping: float = 15.0
+@export var hover_height: float = 0.3
 @export var min_distance: float = 1.0
 @export var max_distance: float = 3.0
+@export var min_hover: float = 0.1
+@export var max_hover: float = 3.0
 
 var _grab_dist: float = 2.0
+var _grab_target_dist: float = 2.0
+var _current_hover: float = 0.3
+var _target_hover: float = 0.3
 
 
 func execute() -> void:
-	_apply_grab_force(_grab_dist, grab_damping, spring_strength)
-
-
-func _apply_grab_force(dist: float, damping: float, strength: float, target_pos: Vector3 = Vector3.INF) -> void:
-	if target_pos == Vector3.INF and camera != null:
-		target_pos = camera.global_transform.origin + camera.global_transform.basis.z * -dist
-	else:
+	var origin_mode = prop.interactor_ref.origin
+	
+	if prop.interactor_ref.get_input_state("grab", "released"):
+		prop.request_camera_release()
+		prop.request_interaction_end()
 		return
 	
+	if prop.interactor_ref.get_input_state("approx", "just_pressed"):
+		_approx_object()
+	if prop.interactor_ref.get_input_state("recede", "just_pressed"):
+		_recede_object()
+	
+	var pos := Vector3.ZERO
+	
+	match origin_mode:
+		prop.interactor_ref.RayOriginMode.CURSOR_MODE:
+			_target_hover = clamp(_target_hover, min_hover, max_hover)
+			_current_hover = lerp(_current_hover, _target_hover, 0.2)
+			pos = _calculate_target_pos()
+		prop.interactor_ref.RayOriginMode.CAMERA_MODE:
+			_grab_target_dist = clamp(_grab_target_dist, min_distance, max_distance)
+			_grab_dist = lerp(_grab_dist, _grab_target_dist, 0.1)
+			pos = Vector3.INF
+		_:
+			return
+	
+	_create_joint(_grab_dist, grab_damping, spring_strength, pos)
+
+
+func _create_joint(dist: float, damping: float, strength: float, target_pos: Vector3 = Vector3.INF) -> void:
+	if target_pos == Vector3.INF and camera != null:
+		target_pos = camera.global_transform.origin - camera.global_transform.basis.z * dist
+	
 	var diff: Vector3 = target_pos - prop.target_body.global_transform.origin
-	var force: Vector3 = _spring(diff, prop.target_body.linear_velocity, strength, damping)
+	var vel: Vector3 = prop.target_body.linear_velocity
+	var force: Vector3 = (diff * strength) - (vel * damping)
 	
 	prop.target_body.apply_central_force(force)
 
 
-func _spring(diff: Vector3, vel: Vector3, strength: float, damping: float) -> Vector3:
-	return (diff * strength) - (vel * damping)
+func _calculate_target_pos() -> Vector3:
+	var mouse_pos = prop.interactor_ref.get_viewport().get_mouse_position()
+	var from = camera.project_ray_origin(mouse_pos)
+	var normal = camera.project_ray_normal(mouse_pos)
+	var rest_y = prop.target_rest_pos_y + _current_hover
+	var plane = Plane(Vector3.UP, rest_y)
+	var intersection = plane.intersects_ray(from, normal)
+	
+	if intersection:
+		return intersection
+	return prop.target_body.global_position
+
+
+func _approx_object() -> void:
+	_target_hover += 0.3
+	_grab_target_dist -= 0.5
+
+
+func _recede_object() -> void:
+	_target_hover -= 0.3
+	_grab_target_dist += 0.5
