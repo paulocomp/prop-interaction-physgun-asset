63e543237bda21ac9a241fd9a3850cb7cb896ac8
paulocomp <paulocorreacomp@gmail.com>
2026-03-15 19:21:34 -0300

    refactor: add input actions easy setup

--- addons/showcase/first-person-demo/character_body_3d_fps.gd
+++ addons/showcase/first-person-demo/character_body_3d_fps.gd
@@ -4,6 +4,13 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const PITCH_LIMIT = 80.0 # deg
 
+@export var input_actions: Dictionary = {
+	"move_up": "move_up",
+	"move_down": "move_down",
+	"move_left": "move_left",
+	"move_right": "move_right"
+}
+
 @export var mouse_sensitivity : float = 3.5
 @onready var _camera_anchor : Node3D = %CameraAnchorFPS
 var camera_mov_enabled: bool = true
@@ -17,7 +24,11 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var input_dir := Input.get_vector(input_actions["move_left"], 
+					  input_actions["move_right"],
+					  input_actions["move_up"],
+					  input_actions["move_down"])
+	
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if direction:
@@ -37,7 +48,10 @@ func _input(event: InputEvent) -> void:
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED and camera_mov_enabled:
+	# camera_mov_enabled is used to toggle camera movement on and off
+	if event is InputEventMouseMotion \
+	and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED \
+	and camera_mov_enabled:
 		rotate_y(-event.relative.x * mouse_sensitivity * 0.001)
 		_camera_anchor.rotate_x(-event.relative.y * mouse_sensitivity * 0.001)
 		_camera_anchor.rotation.x = clamp(
